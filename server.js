89e40c2146d531f4573655daf8e3daf7d82b3ec2
domanchi <domanchi@umich.edu>
2015-03-29 12:51:31 -0400

    env + gitignore configured, session WIP

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var app = express();							//   enable express module
 app.set('views', __dirname + '/views');			//   set default "views" directory
 app.set('view engine', 'jade');					//   enable jade.
 
+var env = (function() {
+	var Habitat = require('habitat');
+	Habitat.load();
+	return new Habitat();
+}());
+
+var session = require('express-session');
+app.use(session({secret: env.get("SESSION_SECRET")}));
+var sess;
+
 var bodyParser = require('body-parser');		//   for express handling POST
 app.use(bodyParser.urlencoded({ extended : false }));
 var search_str = "";
@@ -15,39 +25,47 @@ var key = "";
 var expire_time = new Date();
 
 var api = require('./assets/javascripts/UMapi.js');
-var core_classes = ['TO 300', 'BE 300', 'STRATEGY 425'];
+var core_classes = ['TO 300', 'BE 300', 'STRATEGY 390'];
 var subject_info = null;
 api.set_core_classes(core_classes);
 
 const PORT = 3000;
 
+auth.testing();
+
 //   Routing Requests
 app.get('/', function (request, response) {
-	pref_bits = 0;			//   reset search preferences
-	if(expire_time >= new Date()) {
-		//   Cached Data
-		response.render('home', {data: {'subjects': core_classes, 'sections': subject_info}});
-		return;
-	}
-	async.waterfall([
-		function(callback) {
-			auth.get_access_token(function(val, time) {
-				//   Want to save a local copy of keys, for future access.
-				key = val;
-				expire_time = time;
-				callback(null);
-			});
-		},
-		function(callback) {
-			api.set_api_key(key);
-			api.get_subject_info(function(data) {
-				subject_info = data;
-				callback(null);
-			});
-		}, function(callback) {
+	sess = request.session;
+	
+	if(sess.user) {
+		pref_bits = 0;			//   reset search preferences
+		if(expire_time >= new Date()) {
+			//   Cached Data
 			response.render('home', {data: {'subjects': core_classes, 'sections': subject_info}});
+			return;
 		}
-	]);
+		async.waterfall([
+			function(callback) {
+				auth.get_access_token(function(val, time) {
+					//   Want to save a local copy of keys, for future access.
+					key = val;
+					expire_time = time;
+					callback(null);
+				});
+			},
+			function(callback) {
+				api.set_api_key(key);
+				api.get_subject_info(function(data) {
+					subject_info = data;
+					callback(null);
+				});
+			}, function(callback) {
+				response.render('home', {data: {'subjects': core_classes, 'sections': subject_info}});
+			}
+		]);
+	} else {
+		response.redirect('/dashboard');
+	}
 });
 
 app.post('/', function(request, response) {
@@ -59,6 +77,22 @@ app.post('/', function(request, response) {
 		pref_bits += Math.pow(2, parseInt(arr[i].substring(6).split(',')[1]) - 1);
 	}
 	response.redirect('/search');
+});
+
+//   TODO: Determine whether can use co-sign?
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+app.post('/login', function(req, res) {
+	//   OMG, so unsecure.
+	sess = req.session;
+	sess.user = req.body.username;
+	res.end('done');
+});
+
+app.get('/dashboard', function(req, res) {
+	res.render('dashboard');
 });
 
 app.get('/search', function(req, res) {
